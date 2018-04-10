367330eec67b5b11fb1d6a486e4fefe2fdb9c31f
Darryl Wright <dwright04@qub.ac.uk>
2018-04-10 11:33:21 +0100

    Add syntax error fix from last push

--- tools/classify.py
+++ tools/classify.py
@@ -657,13 +657,13 @@ def main():
         output = open(outputFile, "w")
         files = Files[0]
         pred = preds[0]
-        if Ys ! = []:
+        if Ys != []:
           y = Ys[0]
           for i,prediction in enumerate(pred):
               output.write(files[i].rstrip() + "," + str(prediction) + "," + str(y[i]) + "\n")
         else:
           for i,prediction in enumerate(pred):
-              output.write(files[i].rstrip() + "," + str(prediction) + "\n")        
+              output.write(files[i].rstrip() + "," + str(prediction) + "\n")
         output.close()
             
     if byName:
